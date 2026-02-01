95e5a7b6d520ef4c0ead26939cad7244d8cbd348
Alexandre Rodrigues <alexandrejmr96@gmail.com>
2026-02-01 15:15:45 +0000

    a

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
-  base: 'alexandrejmr22.github.io/BCS26',
+  base: '/alexandrejmr22.github.io/BCiS26',
   plugins: [react(), tailwindcss()],
 })
